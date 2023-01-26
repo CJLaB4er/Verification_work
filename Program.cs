19097a4e0f62837bc5bc907243219787c85ac5b3
Bronislav <CJLaB4er@gmail.com>
2023-01-27 00:51:03 +0300

    Добавлены методы: GetArray, FillArray, PrintArray. Сделаны описания этих методов в Readme.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Перед выполнением кода, очищаем консоль...
+Console.Clear();
+
+// Метод для создания строкового массива определенного размера...
+string[] GetArray(int size)
+{
+    string[] newArray = new string[size];
+    return newArray;
+}
+
+string[] FillArray(string[] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.WriteLine("Введите строковое значение...");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+
+// Метод для вывода в консоль элементов массива...
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write(array[i]);
+    }
+}
+
+string[] testArray = FillArray(GetArray(5));
+PrintArray(testArray);
